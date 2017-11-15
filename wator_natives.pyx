6ebc1336e624a4d5e96244463c85a6a1723ae9d8
Miroslav Kravec <kravec.miroslav@gmail.com>
2017-11-15 18:12:05 +0100

    MI-PYT - ukol 07:  generate_creatures - partially migrated to cython data structures

--- wator_natives.pyx
+++ wator_natives.pyx
@@ -1,8 +1,18 @@
 import numpy
 from random import randrange
 
-def generate_creatures(shape, nfish, nsharks, age_fish, age_shark):
-    creatures = numpy.zeros(shape)
+from libc.stdlib cimport rand, srand
+from libc.time cimport time
+
+cimport numpy
+
+cpdef numpy.ndarray[numpy.int64_t, ndim=2] generate_creatures(
+        tuple shape, int nfish, int nsharks, int age_fish, int age_shark
+    ):
+    cdef numpy.ndarray[numpy.int64_t, ndim=2] creatures
+    cdef int count, r, i
+
+    creatures = numpy.zeros(shape, dtype='int64')
     count = shape[0] * shape[1]
 
     if count < nsharks + nfish:
@@ -11,8 +21,9 @@ def generate_creatures(shape, nfish, nsharks, age_fish, age_shark):
     all_points = creatures.view()
     all_points.shape = (count,)
 
+    srand(time(NULL))
     for i in range(0,count):
-        r = randrange(0,count-i)
+        r = rand() % (count-i)
 
         if r < nfish:
             all_points[i] = randrange(1,age_fish)
